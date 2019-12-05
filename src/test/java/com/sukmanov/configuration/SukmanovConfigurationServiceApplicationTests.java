cd9da8ca52f497dae479bf30d26ec4725826f505
boggie <sukmanov.b@gmail.com>
2019-12-05 21:34:08 +0100

    initial setup

---
+++ src/test/java/com/sukmanov/configuration/SukmanovConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sukmanov.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukmanovConfigurationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
